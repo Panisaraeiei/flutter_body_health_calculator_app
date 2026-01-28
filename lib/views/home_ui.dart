d591a6091e518ab7ea59feaadcb8787040825099
Panisaraeiei <s6619410019@sau.ac.th>
2026-01-28 15:49:14 +0700

    commit : 03 ทำ NavigationBar ที่ homeui เรียบร้อย

--- lib/views/home_ui.dart
+++ lib/views/home_ui.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_iot_second_app/views/about_ui.dart';
+import 'package:flutter_iot_second_app/views/bmi_ui.dart';
+import 'package:flutter_iot_second_app/views/bmr_ui.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class HomeUi extends StatefulWidget {
   const HomeUi({super.key});
@@ -8,8 +12,56 @@ class HomeUi extends StatefulWidget {
 }
 
 class _HomeUiState extends State<HomeUi> {
+  //สร้างตัวแปรควบคุม currenindex ของ BarItem
+  int currenIndexStatus = 1;
+
+  //สร้างตัวแปรที่ Widget หน้าจอที่จะมาแสดง body ของ scaffold
+  List<Widget> showBody = [
+    BmiUi(),
+    AboutUi(),
+    BmrUi(),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 9, 82, 116),
+        title: Text(
+          'Body Health Calculator',
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+        centerTitle: true,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: (value) {
+          setState(() {
+            currenIndexStatus = value;
+          });
+        },
+        currentIndex: currenIndexStatus,
+        selectedItemColor: Colors.pink,
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.person_3,
+              ),
+              label: 'BMI'),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.home,
+              ),
+              label: 'Home'),
+          BottomNavigationBarItem(
+              icon: Icon(
+                FontAwesomeIcons.personBooth,
+              ),
+              label: 'BMR'),
+        ],
+      ),
+      body: showBody[currenIndexStatus],
+    );
   }
 }
